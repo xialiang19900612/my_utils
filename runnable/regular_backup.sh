3a6f74f619ccf6dce34d04b12a79cf077105a40c
Liang <1016807272@qq.com>
2013-06-21 15:23:34 +0800

    modified:   runnable/install_log 	modified:   runnable/regular_backup.sh

--- runnable/regular_backup.sh
+++ runnable/regular_backup.sh
@@ -3,4 +3,4 @@ date=`date -I`;
 # backup home
 /bin/tar -cjf /backup/liang_backup$date.tar.bz2 /home/liang
 # romove old data 
-find /backup -type f -mtime +7 -exec rm -f {} \
+find /backup -type f -mtime +7 -exec rm -f {} \;
